ed1a7da45e2193bb8f9348280afb1c8bb5f9e0bb
Leonardo Tavares <98471823+leonardotavares20@users.noreply.github.com>
2024-01-15 17:27:51 -0300

    Estrutura da do Header iniciada

--- avicii/src/components/Header/Header.jsx
+++ avicii/src/components/Header/Header.jsx
@@ -1,7 +1,30 @@
+import logo from "../../../public/logo.svg";
+
 export default function Header() {
   return (
     <header>
-      <h1>Avicii</h1>
+      <nav className="flex justify-around">
+        <div>
+          <img src={logo} alt="Link to home and logo of Avicii.com" />
+        </div>
+        <menu className="flex">
+          <li>Tim Bergling Foundation</li>
+          <li>Avicii Experience</li>
+          <li>Join the Community</li>
+          <li>Shop</li>
+        </menu>
+        <menu className="flex">
+          <li>
+            <a href="">Spotify</a>
+          </li>
+          <li>
+            <a href="">Youtube</a>
+          </li>
+          <li>
+            <a href="">Apple Music</a>
+          </li>
+        </menu>
+      </nav>
     </header>
   );
 }
