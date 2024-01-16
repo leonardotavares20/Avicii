a9c35bc976d9b140e5e7c58e338928eebcba4a65
Leonardo Tavares <98471823+leonardotavares20@users.noreply.github.com>
2024-01-16 02:52:07 -0300

    NavBa e Header quase finalizados

--- avicii/src/components/Header/Header.jsx
+++ avicii/src/components/Header/Header.jsx
@@ -1,27 +1,34 @@
 import logo from "../../../public/logo.svg";
+import { FaSpotify, FaYoutube, FaApple } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header>
-      <nav className="flex justify-around">
-        <div>
+    <header className="w-full flex justify-center">
+      <nav className="w-11/12 grid grid-cols-navbar justify-around items-center h-36">
+        <div className="h-8 flex items-center">
           <img src={logo} alt="Link to home and logo of Avicii.com" />
         </div>
-        <menu className="flex">
+        <menu className="flex text-sm h-8 font-semibold tracking-normal gap-x-14 items-center">
           <li>Tim Bergling Foundation</li>
           <li>Avicii Experience</li>
           <li>Join the Community</li>
           <li>Shop</li>
         </menu>
-        <menu className="flex">
+        <menu className="flex items-center h-8 justify-evenly">
           <li>
-            <a href="">Spotify</a>
+            <a href="">
+              <FaSpotify size={19} />
+            </a>
           </li>
           <li>
-            <a href="">Youtube</a>
+            <a href="">
+              <FaYoutube size={19} />
+            </a>
           </li>
           <li>
-            <a href="">Apple Music</a>
+            <a href="">
+              <FaApple size={19} />
+            </a>
           </li>
         </menu>
       </nav>
--- avicii/tailwind.config.js
+++ avicii/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        navbar: "1fr 4fr .6fr",
+      },
+      letterSpacing: {
+        normal: "2px",
+      },
+    },
   },
   plugins: [],
 };
